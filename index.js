cd20ce270fffa8b25c3f28c6df720c2dc30f10a2
sonia <kechits@yahoo.com>
2020-08-31 18:52:33 +0200

    ajout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //AU SCROLL FAIRE APPARAITRE LA BARRE DE NAVIGATION DU HAUT
 
-// REMARQUE: ICI ON DEMANDE AUX DEUX FONCTIONS DE S'ACTIVER AU MOMENT DU SCROLL
+// REMARQUE: GRACE A window.onscroll ON DEMANDE AUX DEUX FONCTIONS DE S'ACTIVER AU MOMENT DU SCROLL
 window.onscroll = function() {scrollFunction(), scrollFunctionBis()};
 
 //CREATION DE LA FONCTION
@@ -8,9 +8,11 @@ function scrollFunction() {
     // QUAND L'UTILISATEUR SCROLL VERS LE BAS A 20PX DU HAUT DE LA PAGE, FAIRE APPARAITRE LA NAV
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("nav").style.top = "0";
+  //  TEST: DONNER UNE OPACITE A LA NAV AVC BOUTON OPACITE 1 (CSS) POUR LAISSER BOUTON VISIBLE AVANT SCROLL: ECHECCCCC!
+  //  document.getElementById("nav").style.opacity = "0";
   } else {
-      //ICI ON CACHE LA NAV
-    document.getElementById("nav").style.top = "-63px";
+      //ICI ON CACHE LA NAV A -68PX TOP
+    document.getElementById("nav").style.top = "-68px";
   }
 }
 
@@ -24,7 +26,7 @@ function scrollFunctionBis() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("nav_bas_page").style.bottom = "0";
   } else {
-      //ICI ON CACHE LA NAV
+      //ICI ON CACHE LA NAV A -80PX
     document.getElementById("nav_bas_page").style.bottom = "-80px";
   }
 }
